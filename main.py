5016923370498d05c83bf48fe3f7fb387779f1ff
qobgry <86515170+qobgry@users.noreply.github.com>
2023-03-01 16:09:44 -0500

    Update main.py
    Added a function to draw tiles on the square base. Added shuffle() function to shuffle the numbers placed on the square tiles in the square box.

--- main.py
+++ main.py
@@ -46,3 +46,42 @@ def click(x, y):
         hide[spot] = False
         hide[mark] = False
         state['mark'] = None
+        
+# define function
+# to draw tiles
+# on the square base
+def draw():
+    clear()
+    goto(0, 0)
+    stamp()
+
+    for count in range(64):
+        if hide[count]:
+            x, y = Coordinates(count)
+            Square(x, y)
+
+    mark = state['mark']
+
+    if mark is not None and hide[mark]:
+        x, y = Coordinates(mark)
+        up()
+        goto(x + 2, y)
+        color('black')
+        write(tiles[mark], font=('Arial', 30, 'normal'))
+
+    update()
+    ontimer(draw, 10)
+
+
+tiles = list(range(32)) * 2
+state = {'mark': None}
+hide = [True] * 64
+
+# shuffle the
+# numbers placed inside
+# the square tiles
+shuffle(tiles)
+tracer(False)
+onscreenclick(click)
+draw()
+done()
